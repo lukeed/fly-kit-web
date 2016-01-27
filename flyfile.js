ba8cf8c162471f6e4ffd3dbe5fd6d525acf6d268
luke <luke.edwards05@gmail.com>
2016-01-27 00:36:45 -0800

    add autoprefixer settings

--- flyfile.js
+++ flyfile.js
@@ -132,7 +132,17 @@ export function * styles() {
 		.source(paths.styles.src)
 		.sass({outputStyle: 'compressed'})
 		.autoprefixer({
-			browsers: ['Firefox <= 20']
+			browsers: [
+				'ie >= 10',
+				'ie_mob >= 10',
+				'ff >= 30',
+				'chrome >= 34',
+				'safari >= 7',
+				'opera >= 23',
+				'ios >= 7',
+				'android >= 4.4',
+				'bb >= 10'
+			]
 		})
 		.concat('main.min.css')
 		.target(paths.styles.dest);
