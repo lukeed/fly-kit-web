88f9c53b8d3e991f1b58ab928a85c76e08d0e6d5
luke <luke.edwards05@gmail.com>
2016-02-07 13:40:00 -0800

    service worker registration/attachment (prod only)

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,15 @@
 import test from './test';
 import test2 from './test2';
+import {isOk, onSuccess, onError} from './sw';
 
+// Run the service worker if we determine it's safe to
+// Note: this will Error during development because
+// 'service-worker.js' only exists after production builds.
+if (isOk) {
+	navigator.serviceWorker.register('service-worker.js').then(onSuccess).catch(onError);
+}
+
+// Custom JS goes here!
 const name = 'Johnny';
 console.log(`Heeeeere's ${name}!!`);
 
